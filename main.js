f24a519c4942ebbc66e363e6bf679cb64ec320cf
0xromrom <roman0811@live.nl>
2022-12-28 16:09:22 -0800

    Animation working

--- main.js
+++ main.js
@@ -1,5 +1,51 @@
 //Landing page image cycle
 
+const imageLinks = [
+  "./img/cover/img1.jpg",
+  "./img/cover/img2.jpg",
+  "./img/cover/img3.jpg",
+  "./img/cover/img4.jpg",
+  "./img/cover/img5.png",
+];
+
+window.onload = () => {
+  cycleImages();
+};
+
 const landingImg = document.querySelector(".cycle-image");
 
-const cycleImages = () => {};
+const transitionImage = (imageSrc) => {
+  landingImg.classList.remove("smooth-transition");
+  landingImg.classList.add("smooth-transition");
+  setTimeout(() => {
+    landingImg.src = imageSrc;
+  }, 1000);
+  setTimeout(() => {
+    landingImg.classList.remove("smooth-transition");
+  }, 2500);
+};
+
+const cycleImages = () => {
+  const cycle = () => {
+    setTimeout(() => {
+      landingImg.src = imageLinks[0];
+    }, 0);
+    setTimeout(() => {
+      transitionImage(imageLinks[1]);
+    }, 8000);
+    setTimeout(() => {
+      transitionImage(imageLinks[2]);
+    }, 16000);
+    setTimeout(() => {
+      transitionImage(imageLinks[3]);
+    }, 24000);
+    setTimeout(() => {
+      transitionImage(imageLinks[4]);
+    }, 32000);
+  };
+  cycle();
+
+  setInterval(() => {
+    cycle();
+  }, 33000);
+};
