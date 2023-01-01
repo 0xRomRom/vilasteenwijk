45e28c90893d7528fc5a8d1c317cee15b755dfee
0xRomRom <roman0811@live.nl>
2023-01-02 00:07:08 +0100

    Added rooms animations

--- js/kamersPrijzen.js
+++ js/kamersPrijzen.js
@@ -2,3 +2,98 @@ window.onload = () => {
     const pageHero = document.querySelector('.page-hero');
     pageHero.classList.add('active');
 }
+
+//Animate doors
+
+const imageBox = document.querySelectorAll('.home-img-box');
+const doorImage1 = document.querySelector('.img1');
+const doorImage2 = document.querySelector('.img2');
+const doorImage3 = document.querySelector('.img3');
+const doorImage4 = document.querySelector('.img4');
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+const box3 = document.querySelector('.box3');
+const box4 = document.querySelector('.box4');
+
+imageBox.forEach((box) => {
+    box.addEventListener('click', (e) => {
+
+        if(e.target.classList[1] === 'img1') {
+        doorImage1.src = './img/animated-door.gif';
+        box.disabled = true;
+        box2.classList.add('fadeOut');
+        box3.classList.add('fadeOut');
+        box4.classList.add('fadeOut');
+        setTimeout(() => {
+            kamerModal.classList.add('fadeIn');
+        }, 1000);
+        setTimeout(() => {
+            doorImage1.src = './img/closed-door.jpg';
+            box.disabled = false;
+        }, 2000)
+        }
+
+        if(e.target.classList[1] === 'img2') {
+        doorImage2.src = './img/animated-door.gif';
+        box.disabled = true;
+        box1.classList.add('fadeOut');
+        box3.classList.add('fadeOut');
+        box4.classList.add('fadeOut');
+        setTimeout(() => {
+            kamerModal.classList.add('fadeIn');
+        }, 1000);
+        setTimeout(() => {
+            doorImage2.src = './img/closed-door.jpg';
+            box.disabled = false;
+        }, 2000)
+        }
+
+        if(e.target.classList[1] === 'img3') {
+        doorImage3.src = './img/animated-door.gif';
+        box.disabled = true;
+        box1.classList.add('fadeOut');
+        box2.classList.add('fadeOut');
+        box4.classList.add('fadeOut');
+        setTimeout(() => {
+            kamerModal.classList.add('fadeIn');
+        }, 1000);
+        setTimeout(() => {
+            doorImage3.src = './img/closed-door.jpg';
+            box.disabled = false;
+        }, 2000)
+       
+        }
+        if(e.target.classList[1] === 'img4') {
+            doorImage4.src = './img/animated-door.gif';
+            box.disabled = true;
+            box1.classList.add('fadeOut');
+            box2.classList.add('fadeOut');
+            box3.classList.add('fadeOut');
+            setTimeout(() => {
+                kamerModal.classList.add('fadeIn');
+            }, 1000);
+            setTimeout(() => {
+                doorImage4.src = './img/closed-door.jpg';
+                box.disabled = false;
+            }, 2000)
+        }
+
+    });
+})
+
+
+const kamerModal = document.querySelector('.kamer-modal');
+const closeKamerModal = document.querySelector('.close-kamer-btn');
+
+closeKamerModal.addEventListener('click', () => {
+kamerModal.classList.add('transitionOut');
+kamerModal.classList.remove('fadeIn');
+setTimeout(() => {
+    kamerModal.classList.remove('transitionOut');
+},1000)
+box1.classList.remove('fadeOut');
+box2.classList.remove('fadeOut');
+box3.classList.remove('fadeOut');
+box4.classList.remove('fadeOut');
+
+});
