a8f9cb368ce2198cf7dc0ac6a7202dbe08f82b74
0xRomRom <roman0811@live.nl>
2023-01-03 22:59:43 +0100

    Responsive changes

--- js/fotos.js
+++ js/fotos.js
@@ -2,6 +2,7 @@ window.onload = () => {
   const pageHero = document.querySelector(".page-hero");
   pageHero.classList.add("active");
 
+  //Grid layout
   const elem = document.querySelector(".grid");
   const msnry = new Masonry(elem, {
     // options
@@ -9,3 +10,19 @@ window.onload = () => {
     gutter: 1,
   });
 };
+
+//Toggle fullscreen image
+const overlayContainer = document.querySelector(".image-overlay");
+const overlayImage = document.querySelector(".overlay-image");
+const gridItem = document.querySelectorAll(".grid-item");
+gridItem.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let src = e.srcElement.currentSrc.toString();
+    overlayImage.src = src;
+    overlayContainer.classList.remove("hidden");
+  });
+});
+
+overlayContainer.addEventListener("click", () => {
+  overlayContainer.classList.add("hidden");
+});
