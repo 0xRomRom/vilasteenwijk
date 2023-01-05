d9c3c87d3b76736fd0ada3d046fba6ca320c4342
0xRomRom <roman0811@live.nl>
2023-01-05 22:48:53 +0100

    Contact form linked and animated

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,71 @@
 window.onload = () => {
-    const pageHero = document.querySelector('.page-hero');
-    pageHero.classList.add('active');
-}
+  const pageHero = document.querySelector(".page-hero");
+  pageHero.classList.add("active");
+};
+
+const verzendenButton = document.querySelector(".verzenden");
+
+const emailInput = document.querySelector(".sender-email");
+const naamInput = document.querySelector(".sender-naam");
+const messageInput = document.querySelector(".textbox");
+const succesMessage = document.querySelector(".succes");
+
+verzendenButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (messageInput.value.length < 10) {
+    messageInput.classList.add("shake");
+    verzendenButton.disabled = true;
+    setTimeout(() => {
+      messageInput.classList.remove("shake");
+      verzendenButton.disabled = false;
+    }, 1000);
+    return;
+  }
+
+  if (naamInput.value.length < 1) {
+    naamInput.classList.add("shake");
+    verzendenButton.disabled = true;
+    setTimeout(() => {
+      naamInput.classList.remove("shake");
+      verzendenButton.disabled = false;
+    }, 1000);
+    return;
+  }
+
+  if (emailInput.value.length < 5) {
+    emailInput.classList.add("shake");
+    verzendenButton.disabled = true;
+    setTimeout(() => {
+      emailInput.classList.remove("shake");
+      verzendenButton.disabled = false;
+    }, 1000);
+    return;
+  }
+
+  const params = {
+    from_name: naamInput.value,
+    reply_email: emailInput.value,
+    message: messageInput.value,
+  };
+  emailjs.send("service_syt8eao", "Standaard", params, "1ExHLcqOKMpwg2JF5");
+
+  setTimeout(() => {
+    messageInput.classList.add("slideOut");
+  }, 500);
+
+  setTimeout(() => {
+    naamInput.classList.add("slideOut");
+  }, 650);
+
+  setTimeout(() => {
+    emailInput.classList.add("slideOut");
+  }, 750);
+  setTimeout(() => {
+    verzendenButton.classList.add("slideOut");
+  }, 850);
+  setTimeout(() => {
+    succesMessage.classList.remove("hidden");
+    succesMessage.classList.add("slideIn");
+  }, 1000);
+});
