d2c5de19b68b647c86c1c1005f7e2e1608ac02c1
0xromrom <roman0811@live.nl>
2023-01-07 11:20:17 -0800

    Removed unused javascript

--- js/kamersPrijzen.js
+++ js/kamersPrijzen.js
@@ -271,15 +271,3 @@ const cycleImages = () => {
   }, 41000);
 };
 cycleImages();
-
-var all = document.getElementsByTagName("*"),
-  i = 0,
-  rect,
-  docWidth = document.documentElement.offsetWidth;
-for (; i < all.length; i++) {
-  rect = all[i].getBoundingClientRect();
-  if (rect.right > docWidth || rect.left < 0) {
-    console.log(all[i]);
-    all[i].style.borderTop = "1px solid red";
-  }
-}
